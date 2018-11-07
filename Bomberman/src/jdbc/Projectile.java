be382233da83075f08df7b2212bc487f54f35954
SneakyBelette <pa.dolle@laposte.net>
2018-11-07 15:50:35 +0100

    hyft

---
+++ Bomberman/src/jdbc/Projectile.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdbc;
+
+/**
+ *
+ * @author pdolle
+ */
+public class Projectile {
+    
+    private static String type;
+    private static int x;
+    private static int y;
+    private static int vitessex;
+    private static int vitessey;
+    private static int hauteur;
+    private static int largeur;
+    private static int numero_lanceur;
+
+    
+    /*constructeur*/
+    public Projectile(int id, String pseudo, int x, int y, int pv, String Arme, int direction, int etat, int munition) {
+        this.id = id;
+        this.pseudo = pseudo;
+        this.x = x;
+        this.y = y;
+        this.pv = pv;
+        this.arme = Arme;
+        this.direction = direction;
+        this.etat = etat;
+        this.munition = munition;
+    }
+    
+}
